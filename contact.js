3ae19046c280918e4f871caa2015f3cc88b2a60e
avpizarro <avpizarro@live.com>
2021-01-18 23:55:37 +1100

    Fix button click

---
+++ contact.js
@@ -0,0 +1,14 @@
+var linkedIn = document.querySelector(".linkedIn");
+var gitHub = document.querySelector(".gitHub");
+// var email = document.querySelector(".email")
+
+linkedIn.addEventListener('click', (e => {
+    window.location = "https://linkedin.com/in/avpizarro"
+}));
+
+gitHub.addEventListener('click', (e => {
+    window.location = "https://github.com/avpizarro"
+}));
+
+// email.addEventListener('click', (e => {
+// }));
